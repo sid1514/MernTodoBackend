c5894db11dba3770dec379683a978fbb88adce6c
Siddharth Santosh Shinde <142812176+sid1514@users.noreply.github.com>
2024-09-10 14:02:55 +0530

    correcting routes

--- routes/route.js
+++ routes/route.js
@@ -8,16 +8,16 @@ route.get("/", async (req, res) => {
 });
 
 route.post("/addtask", async (req, res) => {
-    try {
-        const { taskName, taskDescription } = req.body;
-        const task = new Todo({ taskName, taskDescription });
-        await task.save()
-        res.send("task added successfully")
-    } catch (error) {
-        console.log(error)
-        res.send(error)
-    }
-})
+  try {
+    const { taskName, taskDescription } = req.body;
+    const task = new Todo({ taskName, taskDescription });
+    await task.save();
+    res.send("task added successfully");
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
 route.get("/getTasks", async (req, res) => {
   try {
     const tasks = await Todo.find();
@@ -30,17 +30,26 @@ route.get("/getTasks", async (req, res) => {
 
 route.delete("/deleteTask/:id", async (req, res) => {
   try {
-    const { taskId } = req.params;
-    let Data = await Todo.findOne({ _id: taskId });
-    if (Data) {
-      let id = Data._id;
-      await Todo.findByIdAndDelete(id);
-      res.send("Record removed");
-    } else {
-      res.status(404).send("Task not found");
-    }
+    let id = req.params.id;
+    await Todo.findByIdAndDelete(id);
+    res.send("Record removed");
   } catch (error) {
+    res.status(404).send("Task not found");
     console.log(error.message);
+  }
+});
+
+route.put("/completeTask/:id", async (req, res) => {
+  try {
+    const { taskId } = req.params;
+    const task = await Todo.findById(taskId);
+    if (!task) {
+      return res.status(404).send({ message: "task not found" });
+    }
+    task.completed = true;
+    await task.save();
+  } catch (error) {
+    res.status(500).send({ message: "server error", error: error });
   }
 });
 module.exports = route;
--- schema/todo.js
+++ schema/todo.js
@@ -1,7 +1,11 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 const Todo = mongoose.Schema({
   taskName: { type: String },
   taskDescription: { type: String },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 const Todos = new mongoose.model("Todo", Todo);
 module.exports = Todos;
